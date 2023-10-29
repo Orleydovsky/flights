1a2c88a6df40343c2f4001055c5a32e0c3889001
Orleydo Vargas <orleydovsky@gmail.com>
2023-10-29 15:13:01 -0500

    feat: implement control value accessor class on the form field component

--- src/app/shared/components/form-field/form-field.component.ts
+++ src/app/shared/components/form-field/form-field.component.ts
@@ -1,18 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-form-field',
   standalone: true,
   imports: [CommonModule, MatIconModule, MatInputModule, MatFormFieldModule],
   templateUrl: './form-field.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FormFieldComponent),
+      multi: true,
+    },
+  ],
 })
-export class FormFieldComponent {
+export class FormFieldComponent implements ControlValueAccessor {
   @Input({ required: true }) placeholder!: string;
   @Input({ required: true }) label!: string;
   @Input() icon!: string;
+
+  public isDisabled = false;
+  public value!: string;
+
+  public writeValue(value: string): void {
+    this.value = value;
+  }
+
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  public onChange: (value: string) => void = () => {};
+
+  public onTouched: () => void = () => {};
 }
