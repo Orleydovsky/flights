a73563b185906eb433fb6f5522f961e4e668b812
Orleydo Vargas <orleydovsky@gmail.com>
2023-10-29 13:24:57 -0500

    feat: create a logo component and implement it on the app toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LogoComponent } from './shared/components/logo/logo.component';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    LogoComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
