27491fd6f2c58207e90554e4a381e50b7ac24a86
Orleydo Vargas <orleydovsky@gmail.com>
2023-10-29 23:34:58 -0500

    feat: implement currency service to update flight price values based on the selected currency

--- src/app/modules/search/components/search-results-card/search-results-card.component.ts
+++ src/app/modules/search/components/search-results-card/search-results-card.component.ts
@@ -1,13 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { JourneyStep } from '../../models/search.model';
+import { CurrencyService } from 'src/app/shared/services/currency.service';
 
 @Component({
   selector: 'app-search-results-card',
   templateUrl: './search-results-card.component.html',
 })
-export class SearchResultsCardComponent {
+export class SearchResultsCardComponent implements OnInit {
   @Input({ required: true }) origin!: string;
   @Input({ required: true }) destination!: string;
   @Input({ required: true }) price!: number;
   @Input({ required: true }) flights: JourneyStep[] = [];
+
+  public selectedCurrency!: string;
+
+  constructor(private currencyService: CurrencyService) {}
+
+  ngOnInit(): void {
+    this.currencyService.currency$.subscribe(currency => {
+      this.selectedCurrency = currency;
+    });
+  }
 }
--- src/app/shared/components/currency-select/currency-select.component.ts
+++ src/app/shared/components/currency-select/currency-select.component.ts
@@ -4,13 +4,32 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { CurrencyService } from '../../services/currency.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-currency-select',
   standalone: true,
-  imports: [CommonModule, MatInputModule, MatSelectModule, MatFormFieldModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+  ],
   templateUrl: './currency-select.component.html',
 })
 export class CurrencySelectComponent {
   public currencies = ['MXN', 'COP', 'USD'];
+  public selectedCurrency!: string;
+
+  constructor(private currencyService: CurrencyService) {
+    this.currencyService.currency$.subscribe(currency => {
+      this.selectedCurrency = currency;
+    });
+  }
+
+  updateCurrency() {
+    this.currencyService.setCurrency(this.selectedCurrency);
+  }
 }
